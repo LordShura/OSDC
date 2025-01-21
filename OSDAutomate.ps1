c75698e59256190064875e8cf119cff5a2b11f69
LordShura <50758657+LordShura@users.noreply.github.com>
2025-01-21 17:15:55 -0300

    Update OSDAutomate.ps1

--- OSDAutomate.ps1
+++ OSDAutomate.ps1
@@ -1,102 +1,124 @@
-# Menu interativo
-Write-Host "Escolha uma opcao:" -ForegroundColor Green
-Write-Host "1. Enviar dados do Autopilot" -ForegroundColor Green
-Write-Host "2. Formatar c/autopilot img-off" -ForegroundColor Green
-Write-Host "3. Formatar c/autopilot img-on" -ForegroundColor Green
-Write-Host "4. Formatar s/autopilot img-off" -ForegroundColor Green
-Write-Host "5. Formatar s/autopilot img-on" -ForegroundColor Green
-Write-Host "0. Sair" -ForegroundColor Green
+# Função para reiniciar o sistema
+function Restart-System {
+    Write-Host -ForegroundColor Green "Reiniciando sistema..."
+    Start-Sleep -Seconds 2
+    wpeutil reboot
+}
 
-# Obter escolha do usuário
-$choice = Read-Host "Digite o numero da opcao desejada" 
+# Função para desligar o sistema
+function Shutdown-System {
+    Write-Host -ForegroundColor Green "Desligando sistema..."
+    Start-Sleep -Seconds 2
+    wpeutil shutdown
+}
 
-switch ($choice) {
-    "1" {
-        Write-Host "Opção selecionada: Enviar dados para o Autopilot" -ForegroundColor Yellow
-        X:\OSDCloud\Config\OSDSend.ps1
-        wpeutil reboot
-    }
+# Função principal para processar a escolha
+function Run-Escolha {
+    param (
+        [string]$choice
+    )
 
-    "2" {
-        Write-Host "Opção selecionada: Formatar c/autopilot img-off" -ForegroundColor Yellow
-        X:\OSDCloud\Config\OSDSend.ps1
-        Write-Host -ForegroundColor Green "Iniciando OSDCloud  ..."
-        start-sleep -Seconds 3
+    switch ($choice) {
+        "1" {
+            Write-Host "Opção selecionada: Enviar dados para o Autopilot e reiniciar" -ForegroundColor Yellow
+            & X:\OSDCloud\Config\OSDSend.ps1
+            Restart-System
+        }
 
-        Write-Host -ForegroundColor Green "Importando OSD Modulo Powershell"
-        Import-Module OSD -Force
-        # Configurar o OSDCloud para usar o arquivo WIM
+        "2" {
+            Write-Host "Opção selecionada: Formatar c/autopilot img-off" -ForegroundColor Yellow
+            & X:\OSDCloud\Config\OSDSend.ps1
+            Write-Host -ForegroundColor Green "Iniciando OSDCloud..."
+            Start-Sleep -Seconds 3
+            Start-OSDCloud -FindImageFile
+            Restart-System
+        }
 
-        # Iniciar o Deployment Automático
-        Write-Host -ForegroundColor Green "Iniciando OSD Cloud"
-        Start-OSDCloud -FindImageFile
+        "3" {
+            Write-Host "Opção selecionada: Formatar c/autopilot img-on" -ForegroundColor Yellow
+            & X:\OSDCloud\Config\OSDSend.ps1
+            Write-Host -ForegroundColor Green "Iniciando OSDCloud..."
+            Start-Sleep -Seconds 3
+            Start-OSDCloud
+            Restart-System
+        }
 
-        Write-Host -ForegroundColor Green "Reiniciando ..."
-        start-sleep -Seconds 5
-        wpeutil reboot
-    }
-     "3" {
-        Write-Host "Opção selecionada: Formatar c/autopilot img-off" -ForegroundColor Yellow
-        X:\OSDCloud\Config\OSDSend.ps1
-        Write-Host -ForegroundColor Green "Iniciando OSDCloud  ..."
-        start-sleep -Seconds 3
+        "4" {
+            Write-Host "Opção selecionada: Formatar s/autopilot img-off" -ForegroundColor Yellow
+            Write-Host -ForegroundColor Green "Iniciando OSDCloud..."
+            Start-Sleep -Seconds 3
+            Start-OSDCloud -FindImageFile
+            Restart-System
+        }
 
-        Write-Host -ForegroundColor Green "Importando OSD Modulo Powershell"
-        Import-Module OSD -Force
-        # Configurar o OSDCloud para usar o arquivo WIM
+        "5" {
+            Write-Host "Opção selecionada: Formatar s/autopilot img-on" -ForegroundColor Yellow
+            Write-Host -ForegroundColor Green "Iniciando OSDCloud..."
+            Start-Sleep -Seconds 3
+            Start-OSDCloud
+            Restart-System
+        }
 
-        # Iniciar o Deployment Automático
-        Write-Host -ForegroundColor Green "Iniciando OSD Cloud"
-        Start-OSDCloud
+        "0" {
+            Write-Host "reiniciar..." -ForegroundColor Green
+            exit
+        }
 
-        Write-Host -ForegroundColor Green "Reiniciando ..."
-        start-sleep -Seconds 5
-        wpeutil reboot
-    }
-    "4" {
-        Write-Host "Opção selecionada: Formatar s/autopilot img-on" -ForegroundColor Yellow
-        Write-Host -ForegroundColor Green "Iniciando OSDCloud  ..."
-        start-sleep -Seconds 3
-
-        Write-Host -ForegroundColor Green "Importando OSD Modulo Powershell"
-        Import-Module OSD -Force
-        # Configurar o OSDCloud para usar o arquivo WIM
-
-        # Iniciar o Deployment Automático
-        Write-Host -ForegroundColor Green "Iniciando OSD Cloud"
-        Start-OSDCloud -FindImageFile
-
-        Write-Host -ForegroundColor Green "Reiniciando ..."
-        start-sleep -Seconds 5
-        wpeutil reboot
-    }
-     "5" {
-        Write-Host "Opção selecionada: Formatar s/autopilot img-off" -ForegroundColor Yellow
-        Write-Host -ForegroundColor Green "Iniciando OSDCloud  ..."
-        start-sleep -Seconds 3
-
-        Write-Host -ForegroundColor Green "Importando OSD Modulo Powershell"
-        Import-Module OSD -Force
-        # Configurar o OSDCloud para usar o arquivo WIM
-
-        # Iniciar o Deployment Automático
-        Write-Host -ForegroundColor Green "Iniciando OSD Cloud"
-        Start-OSDCloud
-
-        Write-Host -ForegroundColor Green "Reiniciando ..."
-        start-sleep -Seconds 5
-        wpeutil reboot
-    }
-    "0" {
-        Write-Host "Saindo..." -ForegroundColor Green
-        exit
-    }
-    default {
-        X:\OSDCloud\Config\OSDSend.ps1
-        Write-Host "Opção inválida. Enviar dados para Autopilot. Tente novamente." -ForegroundColor Red
-        start-sleep -Seconds 2
-        cls
-        irm https://raw.githubusercontent.com/LordShura/OSDC/refs/heads/main/OSDAutomate.ps1 | iex
-        exit 
+        default {
+            Write-Host "Opção inválida. Tente novamente." -ForegroundColor Red
+            Start-Sleep -Seconds 2
+            cls
+        }
     }
 }
+
+# Função para processar a escolha após uma entrada inválida
+function Run-STDEscolha {
+    param (
+        [string]$stdchoic
+    )
+    switch ($stdchoic) {
+        "1" {
+            # Reexibir o menu após a escolha de tentar novamente
+            return
+        }
+
+        "2" {
+            Shutdown-System
+        }
+
+        default {
+            Write-Host "Opção inválida. Saindo..." -ForegroundColor Red
+            Shutdown-System
+        }
+    }
+}
+
+# Loop para exibir o menu até que uma opção válida seja escolhida
+do {
+    # Menu interativo
+    Write-Host "Escolha uma opção:" -ForegroundColor Green
+    Write-Host "1. Enviar dados do Autopilot e reiniciar" -ForegroundColor Green
+    Write-Host "2. Formatar c/autopilot img-off" -ForegroundColor Green
+    Write-Host "3. Formatar c/autopilot img-on" -ForegroundColor Green
+    Write-Host "4. Formatar s/autopilot img-off" -ForegroundColor Green
+    Write-Host "5. Formatar s/autopilot img-on" -ForegroundColor Green
+    Write-Host "0. Sair" -ForegroundColor Green
+
+    # Obter escolha do usuário
+    $choice = Read-Host "Digite o número da opção desejada"
+
+    # Chamar a função com a escolha do usuário
+    Run-Escolha -choice $choice
+
+    # Verificar se a escolha foi inválida (ou se foi pressionado apenas Enter)
+    if ([string]::IsNullOrWhiteSpace($choice)) {
+        Write-Host "1. Tentar novamente." -ForegroundColor Green
+        Write-Host "2. Desligar." -ForegroundColor Red
+        # Obter escolha do usuário
+        $stdchoic = Read-Host "Digite o número da opção desejada"
+
+        # Chamar a função com a escolha do usuário
+        Run-STDEscolha -stdchoic $stdchoic
+    }
+} while ($choice -ne "0")
