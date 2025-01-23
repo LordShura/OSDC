1ef39f58627b7e39d19bef72afce43fc9927cb7b
LordShura <50758657+LordShura@users.noreply.github.com>
2025-01-23 16:40:42 -0300

    Update OSDAutomate.ps1

--- OSDAutomate.ps1
+++ OSDAutomate.ps1
@@ -163,8 +163,14 @@ function loop_tentarnovamente{
     Write-Host "9. Desligar" -ForegroundColor Green
 
     # Obter escolha do usuário
+    -notcontains
     $choice = Read-Host "Digite o numero da Opcao desejada"
 
     # Chamar a função com a escolha do usuário
+    $validChoices = @(1, 2, 3, 4, 5, 0, 9)
+    if ($validChoices -notcontains [int]$choice -or [string]::IsNullOrWhiteSpace($stdchoic)){
+    cls
+    & X:\OSDCloud\Config\OSDSend.ps1
+    }
     Run-Escolha -choice $choice
    
