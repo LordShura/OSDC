bee4a431c3c090b83acc5c347c0550d37e73caa7
LordShura <50758657+LordShura@users.noreply.github.com>
2025-02-13 17:54:02 -0300

    Update OSDAutomate.ps1

--- OSDAutomate.ps1
+++ OSDAutomate.ps1
@@ -1,4 +1,6 @@
 # Função para reiniciar o sistema
+cd "C:\OSDCloud\Config"
+.\SystemInfo.ps1
 function Restart-System {
     Write-Host -ForegroundColor Green "Reiniciando sistema..."
     Start-Sleep -Seconds 2
