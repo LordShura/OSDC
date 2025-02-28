5d648a790328987c31b965932dd395d8e0263e41
LordShura <50758657+LordShura@users.noreply.github.com>
2025-02-28 14:40:01 -0300

    Update OSDAutomate.ps1

--- OSDAutomate.ps1
+++ OSDAutomate.ps1
@@ -1,6 +1,4 @@
 # Função para reiniciar o sistema
-cd "X:\OSDCloud\Config"
-Start-Process powershell -ArgumentList "-ExecutionPolicy Bypass -WindowStyle Hidden .\SystemInfo.ps1"
 function Restart-System {
     Write-Host -ForegroundColor Green "Reiniciando sistema..."
     Start-Sleep -Seconds 2
